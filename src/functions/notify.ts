0285714053919859d33fda65cf2391de38bc1ad3
shio-yaamaa <shio.yama520@gmail.com>
2019-04-04 04:27:59 +0900

    Use incoming webhooks

--- src/functions/notify.ts
+++ src/functions/notify.ts
@@ -1,11 +1,16 @@
+import axios from 'axios';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  const webhookUrl = `https://hooks.slack.com/services/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`;
+  await axios.post(webhookUrl, {
+    text: 'Hello from Lambda'
+  });
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: 'Success',
       input: event,
     }),
   };
