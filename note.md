4ed8867b8b05ea2bbcdbb6397f979de3768f3005
shio-yaamaa <shio.yama520@gmail.com>
2019-04-17 12:27:37 +0900

    Add generate-chart-url function

---
+++ note.md
@@ -0,0 +1,28 @@
+# Example of QuickChart config
+
+## Mixed Chart Types
+
+```javascript
+const c = {
+  type: "bar",
+  data: {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Dogs",
+        data: [50, 60, 70, 180, 190]
+      },
+      {
+        label: "Cats",
+        data: [100, 200, 300, 400, 500]
+      },
+      {
+        type: "line",
+        fill: false,
+        label: "Potatoes",
+        data: [100, 400, 200, 400, 700]
+      }
+    ]
+  }
+};
+```
---
+++ src/functions/generate-chart-url.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+const quickchartUrl = 'https://quickchart.io/chart';
+
+interface DailyStatistics {
+  date: number;
+  availableAmount: number;
+  dailyUsage: number;
+  currentUsage: number;
+}
+
+const generateChartConfig = (data: DailyStatistics[]): Object => {
+  return {};
+};
+
+const generateChartUrl = (data: DailyStatistics[]): string => {
+  const chartConfig = generateChartConfig(data);
+  const chartConfigString = encodeURIComponent(JSON.stringify(chartConfig));
+  return `${quickchartUrl}?c=${chartConfigString}`;
+};
+
+export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Success',
+      input: event,
+    }),
+  };
+};
