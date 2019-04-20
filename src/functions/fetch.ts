c56bd0dc70e795513070d299c3d08cc010f0a8ba
shio-yaamaa <shio.yama520@gmail.com>
2019-04-21 04:49:34 +0900

    Get remaining data instead of total data

--- src/functions/fetch.ts
+++ src/functions/fetch.ts
@@ -9,7 +9,7 @@ Sentry.init({
 });
 
 interface Credential {
-  msn: string;
+  msn: string; // Softbank refers to phone numbers as "msn"
   password: string;
 }
 
@@ -20,9 +20,9 @@ interface DataUsageInfo {
 }
 
 const baseUrl = 'https://my.ymobile.jp/';
-const loginEndpoint = `/muc/d/auth/doLogin/`;
-const topEndpoint = `/muc/d/top/`;
-const contractEndpoint = `/muc/d/webLink/doSend/MWBWL0020`;
+const loginEndpoint = '/muc/d/auth/doLogin/';
+const topEndpoint = '/muc/d/top/';
+const usageEndpoint = '/muc/d/webLink/doSend/MRERE0000';
 
 // Some information is only available in the smartphone page
 const smartphoneUserAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1';
@@ -41,52 +41,6 @@ const login = async (baseRequest: request.RequestAPI<request.Request, request.Co
         if (error) reject(error);
         resolve();
       },
-    );
-  });
-};
-
-const getTotalDataUsage = async (baseRequest: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): Promise<number> => {
-  return new Promise((resolve, reject) => {
-    baseRequest.get(
-      contractEndpoint,
-      (error, _response, body) => {
-        if (error) reject(error);
-
-        const $ = cheerio.load(body);
-        const form = $('form[name="webLink"]').first();
-        const formAction = form.attr('action');
-        const formData: { [key: string]: string } = {};
-        form.children().filter('input').each((_index, input) => {
-          formData[$(input).attr('name')] = $(input).attr('value');
-        });
-
-        baseRequest.post(
-          formAction,
-          {
-            baseUrl: null,
-            form: formData,
-          },
-          (error, _response, body) => {
-            if (error) reject(error);
-
-            const $ = cheerio.load(body);
-            // This might break when the DOM structure of the page changes.
-            // But there are very few IDs or classes that can be used to identify the target element.
-            const contractDetailContainer = $('.contractual_detail');
-            const currentContractTitle = contractDetailContainer.children().filter((_index, element) => {
-              return $(element).text().includes('現在のご契約');
-            }).first();
-            const currentContractWrap = currentContractTitle.next();
-            const dataAmountBox = currentContractWrap.find('.unit-box').eq(1);
-            const dataAmountElement = dataAmountBox.children().eq(0).children().eq(0).children().eq(1);
-            const totalDataUsage = parseFloat(dataAmountElement.text()); // Unit is automatically ignored
-
-            if (isNaN(totalDataUsage)) reject(new Error('Invalid total data amount'));
-
-            resolve(totalDataUsage);
-          },
-        );
-      }
     );
   });
 };
@@ -116,12 +70,56 @@ const getCurrentDataUsage = async (baseRequest: request.RequestAPI<request.Reque
         if (isNaN(currentDataUsage)) reject(new Error('Invalid current data usage'));
 
         resolve(currentDataUsage);
-      }
+      },
     );
   });
 };
 
-const getDataUsageInfoForSingleUser = async () => {
+const getRemainingDataUsage = async (baseRequest: request.RequestAPI<request.Request, request.CoreOptions, request.RequiredUriUrl>): Promise<number> => {
+  return new Promise((resolve, reject) => {
+    baseRequest.get(
+      usageEndpoint,
+      (error, _response, body) => {
+        if (error) reject(error);
+
+        const $ = cheerio.load(body);
+        const form = $('form[name="webLink"]').first();
+        const formAction = form.attr('action');
+        const formData: { [key: string]: string } = {};
+        form.children().filter('input').each((_index, input) => {
+          formData[$(input).attr('name')] = $(input).attr('value');
+        });
+
+        baseRequest.post(
+          formAction,
+          {
+            baseUrl: null,
+            form: formData,
+          },
+          (error, _response, body) => {
+            if (error) reject(error);
+
+            const $ = cheerio.load(body);
+            // This might break when the DOM structure of the page changes.
+            // But there are very few IDs or classes that can be used to identify the target element.
+            const contractInfoTables = $('table.contract-info');
+            const remainingDataUsageRow = contractInfoTables.find('tr').filter((_index, row) => {
+              return $(row).text().includes('追加料金課金までの残りデータ量');
+            }).first();
+            const remainingDataUsageText = remainingDataUsageRow.children().eq(1).text();
+            const remainingDataUsage = parseFloat(remainingDataUsageText.split('（')[0]); // Unit is automatically ignored
+
+            if (isNaN(remainingDataUsage)) reject(new Error('Invalid remaining data usage'));
+
+            resolve(remainingDataUsage);
+          },
+        );
+      },
+    );
+  });
+};
+
+const getDataUsageInfoForSingleUser = async (): Promise<DataUsageInfo> => {
   const jar = request.jar();
 
   const baseRequest = request.defaults({
@@ -133,19 +131,22 @@ const getDataUsageInfoForSingleUser = async () => {
     followAllRedirects: true,
   });
 
-  try {
-    await login(baseRequest, credential);
-    const dataUsageInfo: DataUsageInfo = {
-      total: await getTotalDataUsage(baseRequest),
-      current: await getCurrentDataUsage(baseRequest),
-    }
-  } catch (error) {
-    Sentry.captureException(error);
-  }
+  await login(baseRequest, credential);
+  const currentDataUsage = await getCurrentDataUsage(baseRequest);
+  const remaininDataUsage = await getRemainingDataUsage(baseRequest);
+  return {
+    total: currentDataUsage + remaininDataUsage,
+    current: currentDataUsage,
+  };
 };
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  getDataUsageInfoForSingleUser();
+  try {
+    getDataUsageInfoForSingleUser();
+  } catch (error) {
+    Sentry.captureException(error);
+  }
+  
   return {
     statusCode: 200,
     body: JSON.stringify({
