76c2d8a7ae8fdde801dbda35373122b4a5cb547a
shio-yaamaa <shio.yama520@gmail.com>
2019-05-12 15:58:38 +0900

    Send chart URLs to Slack

--- src/functions/notify.ts
+++ src/functions/notify.ts
@@ -1,19 +1,39 @@
 import axios from 'axios';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import * as Sentry from '@sentry/node';
 
-export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  const webhookUrl = `https://hooks.slack.com/services/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`;
-  const imageUrl = 'https://4.bp.blogspot.com/-ick5MzawERQ/XGjx4C7m8SI/AAAAAAABRcg/3uCtQPn3VxQBvtK8_97xwYDcxPhZgoJHQCLcBGAs/s400/food_soup_pan.png';
+import { User } from '../types';
+
+import JSTDateTime from '../lib/JSTDateTime';
+import DB from '../lib/DB';
+import Chart from '../lib/Chart';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
+
+const notifySingleUser = async (currentDateTime: JSTDateTime, user: User): Promise<void> => {
+  const records = await DB.getUsageRecordsOfMonth(user, currentDateTime);
   const chartBlock = {
     type: 'image',
     block_id: 'chart',
-    image_url: imageUrl,
+    image_url: new Chart(currentDateTime, records).toUrl(), // TODO: Shorten the URL
     alt_text: 'Chart',
   };
-  await axios.post(webhookUrl, {
+  await axios.post(user.slackWebhookUrl, {
     blocks: [chartBlock],
   });
+};
+
+export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  // TODO: Error handling
+  const currentDateTime = new JSTDateTime();
+  const users = await DB.getAllUsers();
+  for (const user of users) {
+    await notifySingleUser(currentDateTime, user);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({
